566c318704f26373cff1dce774878bc7998b5239
Julius Hell <jhell2306@googlemail.com>
2023-11-24 08:15:35 +0100

    Added disk stats

--- main.py
+++ main.py
@@ -27,6 +27,12 @@ def main():
 
         uptime = psutil.boot_time()
 
+        disk = psutil.disk_usage("/")
+
+        disk_size = int(disk.total / 1000 / 1000)
+        disk_usage = int((disk.total - disk.free) / 1000 / 1000)
+        disk_usage_percent = 100 - ((disk.free / disk.total) * 100)
+
         payload = {
             "cpuCoreCount": cpu_count,
             "cpuUsage": cpu_usage,
@@ -35,14 +41,18 @@ def main():
             "hostname": hostname,
             "arch": arch,
             "platform": os,
-            "uptime": int(uptime)
+            "uptime": int(uptime),
+            "diskSize": disk_size,
+            "diskUsed": disk_usage,
+            "diskUsedPercent": disk_usage_percent,
         }
 
         res = post(url=getenv("API_URL"), json=payload, headers={
             "x-api-key": getenv("API_KEY")
         })
 
-        print(res.status_code)
+        if res.status_code is 200:
+            print("Send stats to api")
 
         sleep(refresh_interval)
 
